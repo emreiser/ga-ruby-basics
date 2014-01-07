78932222c2f49c7382432f898cc37e0ee896d40b
emreiser <emilie.reiser@gmail.com>
2014-01-07 11:48:18 -0500

    Created new ruby file and commented

---
+++ basic_ruby.rb
@@ -0,0 +1,38 @@
+# Assigns child's age and prints
+my_child_age = 2
+
+puts "my child's age is #{my_child_age}"
+
+# Assigns my age and prints
+my_age = 32
+puts "my age is #{my_age}"
+
+# Assigns current year and prints
+current_year = 2014
+puts "current year is #{current_year}"
+
+# Calculates child's year of birth and prints (twice)
+child_dob = current_year - my_child_age
+
+puts "child's Date of Birth = #{child_dob}"
+
+puts "child's Date of Birth = #{current_year - my_child_age}"
+
+# Assigns my year of birth based on age and current year
+my_dob = current_year - my_age
+
+# Assigns dad's age
+my_dads_age = 57
+
+# Assigns dad's year of birth
+my_dads_dob = current_year - my_dads_age
+
+#Assigns my first and last name
+my_first_name = "Jen"
+
+my_last_name = "Smith"
+
+#Assigns full name by concatenating first and last names and prints
+my_full_name = my_first_name + " " + my_last_name
+
+puts "my full name is #{my_full_name}"
